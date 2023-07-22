068f60b9a901a94e0801b12c7a0fffb703402a83
Arsenii <arseniy.golubev@list.ru>
2023-07-22 19:00:22 +0300

    add handler i_know

--- handlers/start_handlers.py
+++ handlers/start_handlers.py
@@ -1,6 +1,6 @@
 from aiogram.utils import exceptions
 from create_bot import dp, types, Dispatcher, bot
-from keyboards import get_base_keyboard, first_look_keyboard
+from keyboards import get_base_keyboard, first_look_keyboard, i_known_keyboard
 
 
 # @dp.message_handler(commands='start')
@@ -48,7 +48,21 @@ async def back_to_start(call: types.CallbackQuery):
                               reply_markup=get_base_keyboard())
 
 
+# @dp.message_handler(text='Я знаю, что я хочу')
+async def i_known(message: types.Message):
+    keyboard_to_delete = types.ReplyKeyboardRemove()
+    try:
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id - 1)
+    except exceptions.MessageToDeleteNotFound:
+        # Если кнопка уже удалена, то продолжаем выполнение кода
+
+        pass
+    await message.answer(text="Скорее подбирай или создавай свой курс",
+                         reply_markup=i_known_keyboard())
+
+
 def register_start_handlers(dp: Dispatcher):
     dp.register_message_handler(start, commands='start')
     dp.register_message_handler(first_look, text='Я здесь впервые')
     dp.register_callback_query_handler(back_to_start, text='back_to_start')
+    dp.register_message_handler(i_known, text='Я знаю, что я хочу')
