4aeec990a827e7b37e65780b39b8ca4af7f01917
Tredikt <ivantkacev7810@gmail.com>
2023-12-12 16:56:56 +0300

    Common updates

--- utils/functions/generate_courses_markup.py
+++ utils/functions/generate_courses_markup.py
@@ -7,7 +7,7 @@ async def generate_courses_keyboard(courses_ids_list: list) -> InlineKeyboardMar
     keyboard = InlineKeyboardMarkup()
 
     for num, course_id in enumerate(courses_ids_list):
-        course_name = await db.get_course_name(course_id=course_id) # TODO
+        course_name = await db.get_course_name(course_id=course_id)
         callback_data = f'course_{course_id}'
         button = InlineKeyboardButton(f"{num + 1}. {course_name.title()}", callback_data=callback_data)
         keyboard.add(button)
