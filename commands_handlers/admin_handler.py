fd0c3e9a79bab55a3c0a26c9831ada9ba773186e
Tredikt <ivantkacev7810@gmail.com>
2024-03-13 23:05:55 +0300

    admin_handler

--- commands_handlers/admin_handler.py
+++ commands_handlers/admin_handler.py
@@ -2,6 +2,8 @@ from utils.functions.get_bot_and_db import get_bot_and_db
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.dispatcher import FSMContext
 
+from blanks.bot_markup import owners_keyboard
+
 from config import owners_ID
 
 
@@ -11,4 +13,9 @@ async def admin_handler(message: Message, state: FSMContext):
     if tg_id in owners_ID:
         bot, db = get_bot_and_db()
 
+        await bot.send_message(
+            chat_id=tg_id,
+            text="Админка",
+            reply_markup=owners_keyboard
+        )
 
