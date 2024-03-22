a0f47ad48928ed30f4aa3e03c26ce926bd270adb
mastersurv <mastersurv.nikiforov@yandex.ru>
2024-03-23 01:05:15 +0300

    get_courses_ids changes

--- utils/functions/statistics_to_student.py
+++ utils/functions/statistics_to_student.py
@@ -4,4 +4,4 @@ from utils.functions.get_bot_and_db import get_bot_and_db
 async def statistics_to_student(tg_id: int):
     bot, db = get_bot_and_db()
 
-    courses_ids = await db.get_courses_ids(tg_id=tg_id)
+    courses_ids = await db.get_created_courses_ids(tg_id=tg_id)
